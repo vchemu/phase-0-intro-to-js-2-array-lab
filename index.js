87256ac44b43868885987fa73fbdce2e10444dfd
valerine <valerine.chemutai@student.moringaschool.com>
2023-08-10 00:19:25 +0300

    arrows complete

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+
+function destructivelyAppendCat(name) {
+  cats.push("Ralph")
+  return cats;
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift("Bob")
+  return cats;
+}
+ 
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+  return cats
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+  return cats
+}
+
+function appendCat(name) {
+  const cats1 = [...cats, "Broom"];
+  return cats1;
+}
+
+function prependCat(name) {
+  const cats2 = ["Arnold", ...cats];
+  return cats2;
+}
+
+function removeLastCat() {
+  const cats3 = cats.slice(0, cats.length-1);
+  return cats3;
+}
+
+function removeFirstCat() {
+  const cats4 = cats.slice(1);
+  return cats4;
+}
